df5d979808ebf45a5458807b805343e7504f3a53
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-10-01 15:58:25 +0200

    (build): Remove unused files and add Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+import "./Layout.scss";
+
+export const Layout = () => {
+  return (
+    <>
+      <header>Header</header>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </>
+  );
+};
